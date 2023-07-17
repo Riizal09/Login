f0a1d4076d2b5f9f0759ca05e156b2a1e0cba7ea
Riizal09 <rizalfadilah0908@gmail.com>
2023-07-17 10:07:19 +0700

    login
    first commit

--- app/src/main/java/com/rizalfadilah/login/MenuActivity.java
+++ app/src/main/java/com/rizalfadilah/login/MenuActivity.java
@@ -8,14 +8,16 @@ import android.view.View;
 import android.widget.Button;
 
 public class MenuActivity extends AppCompatActivity {
-
-    private Button _mahasiswa;
-    private Intent _tampilintent;
+    private Button _mahasiswa, _tampilForebutton, _tampilCuacaButton, _tampilImplisit;
+    private Intent _tampilintent, _tampilForexintent, _tampilCuacaIntent, _ImplisitIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        initTampilMahasiswaButton();}
+        initTampilMahasiswaButton();
+        initTampilForexButton();
+        initTampilCuacaButton();
+        inittampilImplisit();}
     private void initTampilMahasiswaButton(){
 
         _mahasiswa = (Button) findViewById(R.id.mahasiswa);
@@ -27,4 +29,39 @@ public class MenuActivity extends AppCompatActivity {
 
             }
         });}
+
+    private void initTampilForexButton()
+    {
+        _tampilForebutton = (Button) findViewById(R.id.tampilForexButton);
+
+        _tampilForebutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _tampilForexintent=new Intent(getApplicationContext(), ForexMainActivity.class);
+                startActivity(_tampilForexintent);
+            }
+        });
+    }
+    private void initTampilCuacaButton(){
+        _tampilCuacaButton = findViewById(R.id.tampilCuacaButton);
+        _tampilCuacaButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v){
+                _tampilCuacaIntent = new Intent(getApplicationContext(), CuacaMainActivity.class);
+            }
+        });
+    }
+    private void inittampilImplisit()
+    {
+        _tampilImplisit = (Button) findViewById(R.id.tampilImplisit);
+
+        _tampilImplisit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                _ImplisitIntent=new Intent(getApplicationContext(), ImplisitIntentMainActivity.class);
+                startActivity(_ImplisitIntent);
+            }
+        });
+    }
 }
